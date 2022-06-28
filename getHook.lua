bc4967fc1372770612c931f3eccb5a1588e8b8e7
SemCoffee <107354386+SemCoffee@users.noreply.github.com>
2022-06-28 17:57:33 -0300

    Create getHook.lua

---
+++ getHook.lua
@@ -0,0 +1,10 @@
+-- Get Hook Adress ARMv7
+-- Pass the name of lib...
+local lib_name = ''
+local lib_address = gg.getRangesList(lib_name)[1]['start']
+gg.setRanges(gg.REGION_C_DATA)
+gg.searchNumber('E51FF004h',gg.TYPE_DWORD)
+local values = gg.getResults(gg.getResultsCount())
+for _,v in pairs(values)do
+    print(string.format('HOOK: %X \n',v.address - il2cpp_address))
+end
